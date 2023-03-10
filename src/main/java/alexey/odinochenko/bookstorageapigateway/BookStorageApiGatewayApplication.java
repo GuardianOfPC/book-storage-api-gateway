585d91787e0ab2360eb1067666034ca8197a3047
Alexey Odinochenko <conik9999@mail.ru>
2023-03-10 16:43:41 +0300

    init: initial gateway impl

---
+++ src/main/java/alexey/odinochenko/bookstorageapigateway/BookStorageApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package alexey.odinochenko.bookstorageapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStorageApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStorageApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alexey/odinochenko/bookstorageapigateway/BookStorageApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package alexey.odinochenko.bookstorageapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStorageApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
